15d6ceda2e029575ad45ddad4cebcbe2f566fb1f
Severin Schols <severin@schols.de>
2016-11-09 10:52:33 +0100

    Client: start new browser before killing the old one

--- client/signman.sh
+++ client/signman.sh
@@ -11,5 +11,7 @@ CONFIG_URL=http://$HOST/api/v1/config/$NODE
 
 DISPLAY_URL=$(curl -s $CONFIG_URL | grep -e "^URL" | awk '{print $2}')
 
-killall midori
-nohup midori --display=:0.0 -e Fullscreen -a $DISPLAY_URL > /dev/null &
+BROWSER_PID=$(pgrep midori)
+nohup midori --display=:0.0 -e Fullscreen -a $DISPLAY_URL > /dev/null 2>/dev/null &
+sleep 1
+kill $BROWSER_PID
