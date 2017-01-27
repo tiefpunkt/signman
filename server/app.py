d405e75c7533d84f2f5b13a0a13700d7190339fd
Peter Froehlich <peter.froehlich@gutefrage.net>
2017-01-27 09:43:11 +0100

    Reformat

--- server/app.py
+++ server/app.py
@@ -14,41 +14,46 @@ admin.add_view(ModelView(SignURL))
 
 DEFAULT_URL = "http://status.munichmakerlab.de"
 
+
 @app.route('/')
 def index():
-	return "Sign Man\n"
+    return "Sign Man\n"
+
 
 @app.route('/api/v1/config/<string:token>', methods=['GET'])
 def get_config(token):
-	lines = []
+    lines = []
 
-	try:
-		sign = Sign.get(token=token)
-		url = sign.getCurrentURL()
-	except Sign.DoesNotExist:
-		lines.append("# Sign not found")
-		# url = DEFAULT_URL
-		url = "http://signman.intern.munichmakerlab.de/nonregistered/%s" % token 
-	except URL.DoesNotExist:
-		lines.append("# No URL for sign found")
-		url = DEFAULT_URL
+    try:
+        sign = Sign.get(token=token)
+        url = sign.getCurrentURL()
+    except Sign.DoesNotExist:
+        lines.append("# Sign not found")
+        # url = DEFAULT_URL
+        url = "http://signman.intern.munichmakerlab.de/nonregistered/%s" % token
+    except URL.DoesNotExist:
+        lines.append("# No URL for sign found")
+        url = DEFAULT_URL
 
-	lines.append("URL %s" % url)
-	lines.append("NODE %s" % token)
-	lines.append("")
-	return "\n".join(lines)
+    lines.append("URL %s" % url)
+    lines.append("NODE %s" % token)
+    lines.append("")
+    return "\n".join(lines)
+
 
 @app.route('/api/v1/sign/<string:token>', methods=['GET'])
 def get_urls(token):
-	try:
-		sign = Sign.get(token=token)
-	except:
-		return "sign not found"
-	return jsonify(sign.activeURLs())
+    try:
+        sign = Sign.get(token=token)
+    except:
+        return "sign not found"
+    return jsonify(sign.activeURLs())
+
 
 @app.route('/nonregistered/<string:token>', methods=['GET'])
 def non_registered_screen(token):
-	return "<html><body>Connected to SignMan<br>Sign not registered<br>Sign Token: %s" % token
+    return "<html><body>Connected to SignMan<br>Sign not registered<br>Sign Token: %s" % token
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
