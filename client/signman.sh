65bad953b1b2d2a6fd50f7b3a755a578f4993e5c
Peter Froehlich <peter.froehlich@gutefrage.net>
2017-01-27 18:46:40 +0100

    Corner case: empty file

--- client/signman.sh
+++ client/signman.sh
@@ -12,7 +12,7 @@ CONFIG_URL=http://$HOST/api/v1/config/$NODE
 
 DISPLAY_URL=$(curl -s $CONFIG_URL | grep -e "^URL" | awk '{print $2}')
 
-if [ ! -e $CURRENT_SIGN_FILE ] || [ `cat $CURRENT_SIGN_FILE` != "$DISPLAY_URL" ]; then
+if [ ! -e $CURRENT_SIGN_FILE ] || [ "`cat $CURRENT_SIGN_FILE`" != "$DISPLAY_URL" ]; then
 
     BROWSER_PID=$(pgrep midori)
     nohup midori --display=:0.0 -e Fullscreen -a $DISPLAY_URL > /dev/null 2>/dev/null &
