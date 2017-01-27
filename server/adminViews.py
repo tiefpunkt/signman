478de1751ecb08168b0a12b0796978f897e67aa4
Peter Froehlich <peter.froehlich@gutefrage.net>
2017-01-27 09:54:52 +0100

    Reformat

--- server/adminViews.py
+++ server/adminViews.py
@@ -1,8 +1,9 @@
 from flask_admin.contrib.peewee import ModelView
 from db import SignURL
 
-class SignView(ModelView):
-	column_list = ('name', 'token')
-	form_excluded_columns = ['last_url', 'last_url_first_send']
 
-	inline_models = (SignURL,)
+class SignView(ModelView):
+    column_list = ('name', 'token')
+    form_excluded_columns = ['last_url', 'last_url_first_send']
+
+    inline_models = (SignURL,)
