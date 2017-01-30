0917a38d68d341245769e89cd7957b67e31b54e4
Peter Froehlich <peter.froehlich@gutefrage.net>
2017-01-30 17:21:00 +0100

    Restart browser if not started

--- client/signman.sh
+++ client/signman.sh
@@ -11,10 +11,10 @@ NODE=$(ip a | grep "link/ether" | awk '{print $2}' | tr -d ":")
 CONFIG_URL=http://$HOST/api/v1/config/$NODE
 
 DISPLAY_URL=$(curl -s $CONFIG_URL | grep -e "^URL" | awk '{print $2}')
+BROWSER_PID=$(pgrep midori)
 
-if [ ! -e $CURRENT_SIGN_FILE ] || [ "`cat $CURRENT_SIGN_FILE`" != "$DISPLAY_URL" ]; then
+if [ ! -e $CURRENT_SIGN_FILE ] || [ "`cat $CURRENT_SIGN_FILE`" != "$DISPLAY_URL" ] || [ ! "$BROWSER_PID" ]; then
 
-    BROWSER_PID=$(pgrep midori)
     nohup midori --display=:0.0 -e Fullscreen -a $DISPLAY_URL > /dev/null 2>/dev/null &
     sleep 2
     kill $BROWSER_PID
